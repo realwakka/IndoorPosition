18d302cc86ea0d3281c6e4eb3abea5cf0918d0a9
realwakka <realwakka@gmail.com>
2014-11-02 13:32:25 +0900

    1411021332

--- app/src/main/java/com/realwakka/indoor/MyActivity.java
+++ app/src/main/java/com/realwakka/indoor/MyActivity.java
@@ -17,62 +17,70 @@ import java.util.TimerTask;
 
 
 public class MyActivity extends Activity{
-    TextView mTextView;
-    TextView mVelocityView;
+    private TextView mTextView;
 
-    SensorManager mSensorManager;
-    SensorFusionListener mListener;
+    private SensorManager mSensorManager = null;
 
-    Sensor mAccelerometer;
-    Sensor mGyroscope;
-    Sensor mMagnetic;
-    Sensor mLinear;
+    // angular speeds from gyro
+    private float[] gyro = new float[3];
 
-    float[] RotationMatrix = new float[9];
-    float[] RotationMatrix2 = new float[16];
+    // rotation matrix from gyro data
+    private float[] gyroMatrix = new float[9];
 
+    // orientation angles from gyro matrix
+    private float[] gyroOrientation = new float[3];
 
-    float[] Inclination = new float[9];
+    // magnetic field vector
+    private float[] magnet = new float[3];
 
-    float[] GyroscopeData = new float[3];
-    float[] MagneticData = new float[3];
-    float[] AccelerometerData = new float[3];
-    float[] LinearData = new float[3];
+    // accelerometer vector
+    private float[] accel = new float[3];
 
-    float[] Velocity = new float[3];
+    // orientation angles from accel and magnet
+    private float[] accMagOrientation = new float[3];
 
-    boolean AccelerometerSet = false;
-    boolean GyroscopeSet = false;
-    boolean MagneticSet = false;
-    boolean LinearSet = false;
+    // final orientation angles from sensor fusion
+    private float[] fusedOrientation = new float[3];
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    // accelerometer and magnetometer based rotation matrix
+    private float[] rotationMatrix = new float[9];
+
+    SensorFusionListener mListener = new SensorFusionListener();
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-        mTextView = (TextView) findViewById(R.id.my_textView);
-        mVelocityView = (TextView) findViewById(R.id.my_velocity);
+        gyroOrientation[0] = 0.0f;
+        gyroOrientation[1] = 0.0f;
+        gyroOrientation[2] = 0.0f;
 
+        // initialise gyroMatrix with identity matrix
+        gyroMatrix[0] = 1.0f; gyroMatrix[1] = 0.0f; gyroMatrix[2] = 0.0f;
+        gyroMatrix[3] = 0.0f; gyroMatrix[4] = 1.0f; gyroMatrix[5] = 0.0f;
+        gyroMatrix[6] = 0.0f; gyroMatrix[7] = 0.0f; gyroMatrix[8] = 1.0f;
+
+        // get sensorManager and initialise sensor listeners
         mSensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
 
-        mListener = new SensorFusionListener();
+        mTextView = (TextView) findViewById(R.id.my_textView);
+
         initListeners();
     }
 
     public void initListeners(){
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensorManager.registerListener(mListener, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(mListener,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_FASTEST);
 
-        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        mSensorManager.registerListener(mListener, mGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(mListener,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
+                SensorManager.SENSOR_DELAY_FASTEST);
 
-        mMagnetic = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        mSensorManager.registerListener(mListener, mMagnetic,SensorManager.SENSOR_DELAY_FASTEST);
-
-        mLinear = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        mSensorManager.registerListener(mListener,mLinear,SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(mListener,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_FASTEST);
     }
+
     private float[] getTrueAcceleration(float[] accelerometervalues, float[] orientationvalues){
         float[] trueacceleration = new float[3];
         trueacceleration[0] =(float) (accelerometervalues[0]*(Math.cos(orientationvalues[2])*Math.cos(orientationvalues[0])+Math.sin(orientationvalues[2])*Math.sin(orientationvalues[1])*Math.sin(orientationvalues[0])) + accelerometervalues[1]*(Math.cos(orientationvalues[1])*Math.sin(orientationvalues[0])) + accelerometervalues[2]*(-Math.sin(orientationvalues[2])*Math.cos(orientationvalues[0])+Math.cos(orientationvalues[2])*Math.sin(orientationvalues[1])*Math.sin(orientationvalues[0])));
@@ -85,73 +93,195 @@ public class MyActivity extends Activity{
     public void onClick(View v){
         switch(v.getId()){
             case R.id.my_reset:
-                Velocity[0]=0f;
-                Velocity[1]=0f;
-                Velocity[2]=0f;
+                for(int i=0;i<velocity.length;i++){
+                    velocity[i] = 0f;
+                    position[i] = 0f;
+                }
                 break;
 
         }
     }
+    float[] gravity = new float[3];
+    float[] linear_acceleration = new float[3];
+    float[] gyro_data = new float[3];
 
+    private float[] getRotationMatrixFromOrientation(float[] o) {
+        float[] xM = new float[9];
+        float[] yM = new float[9];
+        float[] zM = new float[9];
+
+        float sinX = (float)Math.sin(o[1]);
+        float cosX = (float)Math.cos(o[1]);
+        float sinY = (float)Math.sin(o[2]);
+        float cosY = (float)Math.cos(o[2]);
+        float sinZ = (float)Math.sin(o[0]);
+        float cosZ = (float)Math.cos(o[0]);
+
+        // rotation about x-axis (pitch)
+        xM[0] = 1.0f; xM[1] = 0.0f; xM[2] = 0.0f;
+        xM[3] = 0.0f; xM[4] = cosX; xM[5] = sinX;
+        xM[6] = 0.0f; xM[7] = -sinX; xM[8] = cosX;
+
+        // rotation about y-axis (roll)
+        yM[0] = cosY; yM[1] = 0.0f; yM[2] = sinY;
+        yM[3] = 0.0f; yM[4] = 1.0f; yM[5] = 0.0f;
+        yM[6] = -sinY; yM[7] = 0.0f; yM[8] = cosY;
+
+        // rotation about z-axis (azimuth)
+        zM[0] = cosZ; zM[1] = sinZ; zM[2] = 0.0f;
+        zM[3] = -sinZ; zM[4] = cosZ; zM[5] = 0.0f;
+        zM[6] = 0.0f; zM[7] = 0.0f; zM[8] = 1.0f;
+
+        // rotation order is y, x, z (roll, pitch, azimuth)
+        float[] resultMatrix = matrixMultiplication(xM, yM);
+        resultMatrix = matrixMultiplication(zM, resultMatrix);
+        return resultMatrix;
+    }
+    private float[] matrixMultiplication(float[] A, float[] B) {
+        float[] result = new float[9];
+
+        result[0] = A[0] * B[0] + A[1] * B[3] + A[2] * B[6];
+        result[1] = A[0] * B[1] + A[1] * B[4] + A[2] * B[7];
+        result[2] = A[0] * B[2] + A[1] * B[5] + A[2] * B[8];
+
+        result[3] = A[3] * B[0] + A[4] * B[3] + A[5] * B[6];
+        result[4] = A[3] * B[1] + A[4] * B[4] + A[5] * B[7];
+        result[5] = A[3] * B[2] + A[4] * B[5] + A[5] * B[8];
+
+        result[6] = A[6] * B[0] + A[7] * B[3] + A[8] * B[6];
+        result[7] = A[6] * B[1] + A[7] * B[4] + A[8] * B[7];
+        result[8] = A[6] * B[2] + A[7] * B[5] + A[8] * B[8];
+
+        return result;
+    }
+
+    float factor=0.1f;
+    public static final float EPSILON = 0.000000001f;
+
+    private static final float NS2S = 1.0f / 1000000000.0f;
+    private float timestamp;
+    private boolean initState = true;
+
+    private float[] velocity = new float[3];
+    private float[] position = new float[3];
+    public void gyroFunction(SensorEvent event) {
+        // don't start until first accelerometer/magnetometer orientation has been acquired
+        if (accMagOrientation == null)
+            return;
+
+        // initialisation of the gyroscope based rotation matrix
+        if(initState) {
+            float[] initMatrix = new float[9];
+            initMatrix = getRotationMatrixFromOrientation(accMagOrientation);
+            float[] test = new float[3];
+            SensorManager.getOrientation(initMatrix, test);
+            gyroMatrix = matrixMultiplication(gyroMatrix, initMatrix);
+            initState = false;
+        }
+
+        // copy the new gyro values into the gyro array
+        // convert the raw gyro data into a rotation vector
+        float[] deltaVector = new float[4];
+        if(timestamp != 0) {
+            final float dT = (event.timestamp - timestamp) * NS2S;
+            System.arraycopy(event.values, 0, gyro, 0, 3);
+            getRotationVectorFromGyro(gyro, deltaVector, dT / 2.0f);
+        }
+
+        // measurement done, save current time for next interval
+        timestamp = event.timestamp;
+
+        // convert rotation vector into rotation matrix
+        float[] deltaMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(deltaMatrix, deltaVector);
+
+        // apply the new rotation interval on the gyroscope based rotation matrix
+        gyroMatrix = matrixMultiplication(gyroMatrix, deltaMatrix);
+
+        SensorManager.getOrientation(gyroMatrix, gyroOrientation);
+
+
+        float[] result = getTrueAcceleration(linear_acceleration,gyroOrientation);
+        String str ="";
+        for(int i=0;i<result.length;i++)
+            str = str+"\n A"+i+" "+result[i];
+
+
+        for(int i=0;i<result.length;i++) {
+            velocity[i] += result[i];
+            str = str + "\n V" + i + " " + velocity[i];
+        }
+        for(int i=0;i<result.length;i++) {
+            position[i] += velocity[i];
+            str = str + "\n P" + i + " " + position[i];
+        }
+
+        mTextView.setText(str);
+    }
+
+    private void getRotationVectorFromGyro(float[] gyroValues,float[] deltaRotationVector,float timeFactor)
+    {
+        float[] normValues = new float[3];
+
+        // Calculate the angular speed of the sample
+        float omegaMagnitude =
+                (float)Math.sqrt(gyroValues[0] * gyroValues[0] +
+                        gyroValues[1] * gyroValues[1] +
+                        gyroValues[2] * gyroValues[2]);
+
+        // Normalize the rotation vector if it's big enough to get the axis
+        if(omegaMagnitude > EPSILON) {
+            normValues[0] = gyroValues[0] / omegaMagnitude;
+            normValues[1] = gyroValues[1] / omegaMagnitude;
+            normValues[2] = gyroValues[2] / omegaMagnitude;
+        }
+
+        // Integrate around this axis with the angular speed by the timestep
+        // in order to get a delta rotation from this sample over the timestep
+        // We will convert this axis-angle representation of the delta rotation
+        // into a quaternion before turning it into the rotation matrix.
+        float thetaOverTwo = omegaMagnitude * timeFactor;
+        float sinThetaOverTwo = (float)Math.sin(thetaOverTwo);
+        float cosThetaOverTwo = (float)Math.cos(thetaOverTwo);
+        deltaRotationVector[0] = sinThetaOverTwo * normValues[0];
+        deltaRotationVector[1] = sinThetaOverTwo * normValues[1];
+        deltaRotationVector[2] = sinThetaOverTwo * normValues[2];
+        deltaRotationVector[3] = cosThetaOverTwo;
+    }
+    public void calculateAccMagOrientation() {
+        if(SensorManager.getRotationMatrix(rotationMatrix, null, accel, magnet)) {
+            SensorManager.getOrientation(rotationMatrix, accMagOrientation);
+        }
+    }
     class SensorFusionListener implements SensorEventListener{
         @Override
         public void onSensorChanged(SensorEvent event) {
-            if(event.sensor==mAccelerometer){
-                System.arraycopy(event.values,0,AccelerometerData,0,3);
-                AccelerometerSet = true;
-            }else if(event.sensor==mGyroscope){
-                System.arraycopy(event.values,0,GyroscopeData,0,3);
-                GyroscopeSet = true;
-            }else if(event.sensor==mMagnetic) {
-                System.arraycopy(event.values, 0, MagneticData, 0, 3);
-                MagneticSet = true;
-            }else if(event.sensor==mLinear){
-                    System.arraycopy(event.values,0,LinearData,0,3);
-                    LinearSet = true;
+            switch(event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    System.arraycopy(event.values, 0, accel, 0, 3);
+                    calculateAccMagOrientation();
 
-            }else{
+                    final float alpha = 0.8f;
 
-            }
-            if(AccelerometerSet && MagneticSet && LinearSet) {
-                SensorManager.getRotationMatrix(RotationMatrix, null, AccelerometerData, MagneticData);
-                SensorManager.getRotationMatrix(RotationMatrix2, null, AccelerometerData, MagneticData);
+                    gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+                    gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+                    gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
 
-                float[] inv = new float[16];
-                float[] result = new float[4];
-                float[] linear2 = new float[4];
+                    linear_acceleration[0] = event.values[0] - gravity[0];
+                    linear_acceleration[1] = event.values[1] - gravity[1];
+                    linear_acceleration[2] = event.values[2] - gravity[2];
 
-                linear2[0] = LinearData[0];
-                linear2[1] = LinearData[1];
-                linear2[2] = LinearData[2];
+                    break;
 
-                Matrix.invertM(inv,0,RotationMatrix2,0);
+                case Sensor.TYPE_GYROSCOPE:
+                    // process gyro data
+                    gyroFunction(event);
+                    break;
 
-                Matrix.multiplyMV(result,0,inv,0,linear2,0);
-
-                float[] orientation = new float[3];
-                float[] orientation2 = new float[4];
-                SensorManager.getOrientation(RotationMatrix,orientation);
-                SensorManager.getOrientation(RotationMatrix2,orientation2);
-
-                float[] trueaccel = getTrueAcceleration(LinearData,orientation);
-
-                String str = "x : "+result[0]+"\ny : "+result[1]+"\nz : "+(result[2]);
-
-                Velocity[0] += result[0];
-                Velocity[1] += result[1];
-                Velocity[2] += result[2];
-
-                float o_x = (float) Math.toDegrees(orientation2[0]);
-                float o_y = (float) Math.toDegrees(orientation2[1]);
-                float o_z = (float) Math.toDegrees(orientation2[2]);
-
-
-
-                String v_str = "x : "+Velocity[0]+"\ny : "+Velocity[1]+"\nz : "+Velocity[2];
-                v_str = v_str + "\nx : "+o_x+"\ny : "+o_y+"\nz : "+o_z;
-                mTextView.setText(str);
-                mVelocityView.setText(v_str);
-
+                case Sensor.TYPE_MAGNETIC_FIELD:
+                    // copy new magnetometer data into magnet array
+                    System.arraycopy(event.values, 0, magnet, 0, 3);
+                    break;
 
             }
         }
